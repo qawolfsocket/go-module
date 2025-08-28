7f3612ab1783e3d8a3c53c4abaac2135075d8324
qawolfsocket <socket+qawolf1@qawolf.email>
2025-08-28 18:41:45 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 // Package main provides helpers for automated testing
 // with the QA Wolf Socket Go module. It contains subpackages
-// like greet and others to demonstrate publishing and usage.
+// like qawolf_package and others to demonstrate publishing and usage.
 package main
 
 import (
 	"fmt"
 
-	"github.com/qawolfsocket/qawolf-socket-go-module/greet"
+	"github.com/qawolfsocket/qawolf-socket-go-module/qawolf_package"
 )
 
 func main() {
-	fmt.Println(greet.Hello())
+	fmt.Println(qawolf_package.Hello())
 }
