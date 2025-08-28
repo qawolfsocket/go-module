4f8a1c58482d3bf5f84e7e71165f85ec30301367
qawolfsocket <socket+qawolf1@qawolf.email>
2025-08-28 17:14:48 +0000

    Update greet.go

--- greet/greet.go
+++ greet/greet.go
@@ -2,5 +2,5 @@ package greet
 
 // Hello returns a greeting string.
 func Hello() string {
-	return "hello from github.com/qawolfsocket/go-module"
+	return "hello from github.com/qawolfsocket/qawolf-socket-go-module"
 }
