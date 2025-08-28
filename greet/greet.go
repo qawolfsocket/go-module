563792d7c8c720558f718e9110ae653c7bfa0ea7
qawolfsocket <socket+qawolf1@qawolf.email>
2025-08-28 18:40:59 +0000

    Delete greet directory

--- greet/greet.go
+++
@@ -1,8 +0,0 @@
-// Package greet contains simple greeting functions used to
-// demonstrate Go module publishing and versioning.
-package greet
-
-// Hello returns a greeting string.
-func Hello() string {
-	return "hello from github.com/qawolfsocket/qawolf-socket-go-module"
-}
